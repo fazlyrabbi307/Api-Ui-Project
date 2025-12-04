40254c2acbe36cb15b2c77f52aeea9a6e133bccf
MD. Rabbi Hossain <fazlyrabbi307@gmail.com>
2025-12-04 23:40:45 +0600

    update

--- lib/api2/api_2.dart
+++ lib/api2/api_2.dart
@@ -1,6 +1,13 @@
-import 'package:flutter/material.dart';
+import 'dart:convert';
+
+import 'package:http/http.dart' as http;
 
 class PostApi {
 
+   getaData() async{
+    Uri url = Uri.parse("https://appapi.coderangon.com/api/slider");
+    var response = await http.get(url);
+        return ;
+  }
 
 }
